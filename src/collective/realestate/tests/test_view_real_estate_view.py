3a59e0e6aed6cf2213d9d2d71074f6110da88b59
Benoit Suttor <bsuttor@imio.be>
2019-01-06 14:50:28 +0100

    Fixing tests

--- src/collective/realestate/tests/test_view_real_estate_view.py
+++ src/collective/realestate/tests/test_view_real_estate_view.py
@@ -26,7 +26,7 @@ class ViewsIntegrationTest(unittest.TestCase):
             (self.portal['my-realestate'], self.portal.REQUEST),
             name='real-estate-view'
         )
-        self.assertTrue('<div class="figures row">' in view(),
+        self.assertTrue('<div class="figures' in view(),
                         'class figures row not found in real-estate-view')
 
     def test_real_estate_view_in_my_collection(self):
