335ceab62125232dff055d7c3fe57cfb1a611a8d
Benoit Suttor <bsuttor@imio.be>
2019-08-08 10:11:52 +0200

    setup.py is now python black compliant

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='collective.realestate',
-    version='1.0a1',
-    description='Make a sale and rental real estate website',
+    name="collective.realestate",
+    version="1.0a1",
+    description="Make a sale and rental real estate website",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -28,50 +30,50 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone',
-    author='Benoit Suttor',
-    author_email='benoit@suttor.be',
-    url='https://pypi.python.org/pypi/collective.realestate',
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['collective'],
-    package_dir={'': 'src'},
+    keywords="Python Plone",
+    author="Benoit Suttor",
+    author_email="benoit@suttor.be",
+    url="https://pypi.python.org/pypi/collective.realestate",
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["collective"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
-        'collective.themefragments',
-        'collective.themesitesetup',
-        'plone.app.dexterity',
-        'plone.api>=1.8.4',
-        'Products.GenericSetup>=1.8.2',
-        'setuptools',
-        'z3c.jbot',
-        'plone.app.mosaic',
-        'eea.facetednavigation',
-        'collective.dexteritytextindexer',
-        'collective.behavior.gallery',
-        'plone.app.imagecropping',
-        'collective.behavior.banner',
-        'collective.eeafaceted.layoutwidget',
-        'collective.address',
-        'collective.geolocationbehavior',
-        'plone.formwidget.captcha',
-        'ftw.calendar',
-        'collective.autoscaling',
-        'collective.disclaimer',
+        "collective.themefragments",
+        "collective.themesitesetup",
+        "plone.app.dexterity",
+        "plone.api>=1.8.4",
+        "Products.GenericSetup>=1.8.2",
+        "setuptools",
+        "z3c.jbot",
+        "plone.app.mosaic",
+        "eea.facetednavigation",
+        "collective.dexteritytextindexer",
+        "collective.behavior.gallery",
+        "plone.app.imagecropping",
+        "collective.behavior.banner",
+        "collective.eeafaceted.layoutwidget",
+        "collective.address",
+        "collective.geolocationbehavior",
+        "plone.formwidget.recaptcha",
+        "ftw.calendar",
+        "collective.autoscaling",
+        "collective.disclaimer",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
-            'ipdb',
-        ],
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
+            "ipdb",
+        ]
     },
     entry_points="""
     [z3c.autoinclude.plugin]
