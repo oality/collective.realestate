b099865b76c364c1bd6ba78a8311a66f34b9d852
Benoit Suttor <bsuttor@imio.be>
2019-08-08 10:12:28 +0200

    Set python black compliant

--- src/collective/realestate/browser/adapters.py
+++ src/collective/realestate/browser/adapters.py
@@ -4,27 +4,25 @@ from DateTime import DateTime
 from ftw.calendar.browser.calendarupdateview import CalendarJSONSource
 from ftw.calendar.browser.interfaces import IFtwCalendarJSONSourceProvider
 from Products.CMFCore.utils import getToolByName
-from zope.interface import implements
+from zope.interface import implementer
 
 
+@implementer(IFtwCalendarJSONSourceProvider)
 class RealEstateCalendarJSONSource(CalendarJSONSource):
-    implements(IFtwCalendarJSONSourceProvider)
-
     def get_event_brains(self):
         args = {
-            'start': {
-                'query': DateTime(self.request.get('end')), 'range': 'max'},
-            'end': {
-                'query': DateTime(self.request.get('start')), 'range': 'min'}}
+            "start": {"query": DateTime(self.request.get("end")), "range": "max"},
+            "end": {"query": DateTime(self.request.get("start")), "range": "min"},
+        }
 
-        catalog = getToolByName(self.context, 'portal_catalog')
-        portal_calendar = getToolByName(self.context, 'portal_calendar',
-                                        None)
+        catalog = getToolByName(self.context, "portal_catalog")
+        portal_calendar = getToolByName(self.context, "portal_calendar", None)
         if portal_calendar:
-            args['portal_type'] = portal_calendar.getCalendarTypes()
+            args["portal_type"] = portal_calendar.getCalendarTypes()
         return catalog(
-            path={'depth': -1,
-                  'query': '/'.join(
-                    get_first_realestate_obj().getPhysicalPath())},
+            path={
+                "depth": -1,
+                "query": "/".join(get_first_realestate_obj().getPhysicalPath()),
+            },
             **args
         )
